7abc692b2c933f9ef859147a106c9bbe92b98df5
amarten <alexdmarten@gmail.com>
2021-11-13 15:13:38 -0800

    deny blank name entries and prompt again

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,8 +160,16 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
